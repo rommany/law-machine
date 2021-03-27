79e3dcd32a62f546dd0a123c3cbb3800a8738f19
Rommany Allen <rommanyallen@gmail.com>
2021-03-27 11:47:42 +0200

    first commit

---
+++ civilProcedureAct.py
@@ -0,0 +1,9 @@
+import PyPDF2
+filename='Civil Procedure Act.pdf'
+
+pdf_file = open(filename)
+read_pdf = PyPDF2.PdfFileReader(pdf_file)
+number_of_pages = read_pdf.getNumPages()
+page = read_pdf.getPage(0)
+page_content = page.extractText()
+print (page_content)
---
+++ rip_pdf.py
@@ -0,0 +1,68 @@
+import Enum
+
+# Dolis
+# Culpa
+# Culpa
+
+# DolisDirectus
+
+
+# bilateral juristic act
+#
+# consequences
+# legal obligation (perform)
+#   unilateral
+#   bilateral  - synallagmatic - reciprocal
+#   multilateral
+
+contract = {
+
+    'basis': {
+        'animus contrahendi': 'Seriously intend to contract',
+        'ad idem': "Are of one mind as to the contract's material terms,conscious of the fact that their minds have met"
+    },
+
+    'obligation' : {
+        'dare'  : 'give',
+        'facere' : 'do',
+        'non facere' : 'not to do',
+        'warranty' : 'a certain state of affairs exists.'
+    }
+}
+
+delict  = {
+    'damages': [
+        { 'patrimonial':
+            ['medical',
+            'loss of income',
+            'cost of repairs',
+            'loss of amenities']
+        },
+
+        { 'non-patrimonial':
+            ['pain and suffering',
+            'disfigurement','loss of amenities',
+            'injury to personality',
+            ]
+        },
+
+        { 'pure economic harm':[]
+
+        }
+    ]
+}
+
+law = {
+    'bona fides' : 'good faith',
+    'dolis' : 'the direction of the will',
+    'consensus ad idem' : 'mutual agreement',
+    'compus mentus' : 'capacity to enter into contract',
+}
+
+
+
+
+
+
+
+
